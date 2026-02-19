f1fb506bb146823974f599b7b977fe954ff3c344
Vineet Gupta <guptavineet400@gmail.com>
2026-02-19 11:55:04 +0530

    lr fix

--- models/test.js
+++ models/test.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const branchToModel = {
+    lr: 'Question',
     ai: 'AiDepartment',
     che: 'ChemicalDepartment',
     chm: 'ChemistryDepartment',
